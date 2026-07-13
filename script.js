04fca29cf6a4350b663f9f733af3a51b604d5f2e
mparrap-hub <mparrap@fen.uchile.cl>
2026-07-12 20:32:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ function mostrarProducto(data){
             🔄 Nueva consulta
         </button>
 
-        <h3 style="color:#2E7D32;">✅ Producto encontrado</h3>
+        <h3 style="color:#2e7d32;">✅ Producto encontrado</h3>
 
         <p><b>Código escaneado (fabricante)</b><br>
         ${data.codigo}</p>
